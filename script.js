c50d56c9798739468ecc23004f2b86136a865946
LssicS7 <masp.code.40@gmail.com>
2026-05-28 07:00:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-
-document.querySelector('.serchbar form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  const input = document.querySelector('.serchbar input').value.trim();
-  
-  if (input.includes('.') && !input.includes(' ')) {
-    // Es una URL
-    window.location.href = input.startsWith('http') ? input : 'https://' + input;
-  } else {
-    // Es una búsqueda
-    window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(input);
-  }
-});
-
 window.onload = function() {
   document.querySelector('.serchbar input').focus();
+  
+  document.querySelector('.serchbar form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const input = document.querySelector('.serchbar input').value.trim();
+    
+    if (input.includes('.') && !input.includes(' ')) {
+      window.location.href = input.startsWith('http') ? input : 'https://' + input;
+    } else {
+      window.location.href = 'https://www.google.com/search?q=' + encodeURIComponent(input);
+    }
+  });
 }
 
 function actualizarReloj() {
   const ahora = new Date();
-  
   const opciones = { month: 'short', day: 'numeric', hour: 'numeric', minute: '2-digit', hour12: true };
   const formato = ahora.toLocaleString('en-US', opciones);
-  
   document.getElementById('clock').textContent = formato;
 }
 
